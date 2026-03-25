35b5da74edeaa686bc96b6e5dd14daa58036a127
ShadowCat650 <64231061+Shadowcat650@users.noreply.github.com>
2026-03-25 16:40:48 -0700

    fix: make `token_source::create_source` public

--- gcloud-sdk/src/lib.rs
+++ gcloud-sdk/src/lib.rs
@@ -54,7 +54,9 @@ pub mod error;
 mod token_source;
 pub use token_source::auth_token_generator::GoogleAuthTokenGenerator;
 pub use token_source::metadata::Metadata as GceMetadataClient;
-pub use token_source::{BoxSource, ExternalJwtFunctionSource, Source, Token, TokenSourceType};
+pub use token_source::{
+    create_source, BoxSource, ExternalJwtFunctionSource, Source, Token, TokenSourceType,
+};
 
 mod api_client;
 pub use api_client::*;
